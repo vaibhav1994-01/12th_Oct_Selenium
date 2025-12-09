c07b87623a1868b3152ea40e96377ec82349a63f
vaibhav1994-01 <vaibhavgat108@gmail.com>
2025-12-09 18:16:07 +0530

    [Emp123 Vaibhav Gat] : Git test

--- src/test/java/actionClass/DoubleClick.java
+++ src/test/java/actionClass/DoubleClick.java
@@ -20,6 +20,7 @@ public class DoubleClick {
 		WebElement button = driver.findElement(By.xpath("//button[text()='Double-Click Me To See Alert']"));
 		Actions act = new Actions(driver); //by using Actions class we can perform operations like click/double click.
 		act.doubleClick(button).build().perform(); //if .perform() is not added then action is not performed.
+		System.out.println("Git Test");
 	}
 
 }
